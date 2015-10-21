f98d943eb39c4393455047f832d8f6138877ccb7
xiexg <568335129@qq.com>
2015-10-21 11:26:57 +0800

    modify joindemon

---
+++ src/main/java/com/ec/xxg/jdk/thread/gitdemon.java
@@ -0,0 +1,10 @@
+package com.ec.xxg.jdk.thread;
+
+/**
+ * Created by ecuser on 2015/10/21.
+ */
+public class gitdemon {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
